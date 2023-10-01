c953971588ba0d5ce75679f7945e9dc3df677128
Sander2022164 <2022164@student.cct.ie>
2023-10-01 11:34:10 +0100

    First input
    Add scanner and storage first input

---
+++ MiniHomework/src/minihomework/MiniHomework.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihomework;
+
+/**
+ *
+ * @author sande
+ */
+public class MiniHomework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Adding Scanner
+        Scanner kb = new Scanner (System.in);
+  
+        //Prompt first number to the user
+        System.out.println("Please pick a number.");
+        //Getting user input
+        int num1 = kb.nextInt();
+    }
+    
+}
